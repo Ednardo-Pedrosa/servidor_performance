069e4795706d6b00026058e09663750d8a2f3e64
Ednardo Pedrosa <69728490+Ednardo-Pedrosa@users.noreply.github.com>
2023-11-26 22:55:15 -0300

    Update performance.sh

--- performance.sh
+++ performance.sh
@@ -66,6 +66,7 @@ grep "siblings" /proc/cpuinfo | uniq | awk -F ': ' '{print $2}'
 print_separator
 
 # Coleta media de velocidade de escrita da unidade de armazenamento
+echo -e "${CYAN}Teste de velocidade de escrita:${NC}"
 dd if=/dev/sda1 of=/tmp/testfile bs=1M count=1000 conv=fsync
 sleep 3
 dd if=/dev/sda1 of=/tmp/testfile bs=1M count=1000 conv=fsync
